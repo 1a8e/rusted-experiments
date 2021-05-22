839ed811998f8143fdd19e7a82f031c75e28c517
1a8e <blueapple@live.in>
2021-05-22 13:21:34 +0530

    Finsih variables unit from the book

---
+++ variables/target/debug/deps/variables-78310d280fb867ea.d
@@ -0,0 +1,5 @@
+/home/cyan/ccr/rusted-experiments/variables/target/debug/deps/variables-78310d280fb867ea: src/main.rs
+
+/home/cyan/ccr/rusted-experiments/variables/target/debug/deps/variables-78310d280fb867ea.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/home/cyan/ccr/rusted-experiments/variables/target/debug/variables: /home/cyan/ccr/rusted-experiments/variables/src/main.rs
