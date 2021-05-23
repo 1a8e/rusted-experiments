efe0cb11909aeedf6920ff74a1bc0d5173db634f
1a8e <5284980+1a8e@users.noreply.github.com>
2021-05-24 00:11:13 +0530

    Delete temperature/target directory

--- temperature/target/debug/deps/temperature.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Anas\ccr\rusted-experiments\temperature\target\debug\deps\temperature.exe: src\main.rs
-
-C:\Users\Anas\ccr\rusted-experiments\temperature\target\debug\deps\temperature.d: src\main.rs
-
-src\main.rs:
--- temperature/target/debug/temperature.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Anas\ccr\rusted-experiments\temperature\target\debug\temperature.exe: C:\Users\Anas\ccr\rusted-experiments\temperature\src\main.rs
