8f6c951e52614864b3343a0f4b4fd624ac12bb24
1a8e <5284980+1a8e@users.noreply.github.com>
2021-05-24 00:10:11 +0530

    Delete christmas_days/target directory

--- christmas_days/target/release/christmas_days.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Anas\ccr\rusted-experiments\christmas_days\target\release\christmas_days.exe: C:\Users\Anas\ccr\rusted-experiments\christmas_days\src\main.rs
--- christmas_days/target/release/deps/christmas_days.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Anas\ccr\rusted-experiments\christmas_days\target\release\deps\christmas_days.exe: src\main.rs
-
-C:\Users\Anas\ccr\rusted-experiments\christmas_days\target\release\deps\christmas_days.d: src\main.rs
-
-src\main.rs:
