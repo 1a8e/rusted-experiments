11d2a06f97e88ad24c4fb1a40d52c6b5ea1bc7f4
1a8e <blueapple@live.in>
2021-05-24 16:20:14 +0530

    Add newlines to EOF

--- random-stuff/src/lib.rs
+++ random-stuff/src/lib.rs
@@ -1,9 +1,9 @@
-use wasm_bindgen::prelude::*;
-use rand::{thread_rng, Rng};
 use rand::distributions::Alphanumeric;
+use rand::{thread_rng, Rng};
+use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern {
+extern "C" {
     pub fn alert(s: &str);
 }
 
